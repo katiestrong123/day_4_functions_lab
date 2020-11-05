928154ef0273df69a87baf7b6defac4f641d8c9d
Katie Strong <katiestrong123@gmail.com>
2020-11-05 15:03:09 +0000

    end of lab

--- functions_lab.py
+++ functions_lab.py
@@ -10,67 +10,67 @@ tasks = [
 
 # 1. Print a list of uncompleted tasks
 
-# def uncompleted_tasks(list):
-#     for task in list:
-#         if task["completed"] == False:
-#             print(task)
+def uncompleted_tasks(list):
+    for task in list:
+        if task["completed"] == False:
+            print(task)
 
 # uncompleted_tasks(tasks)
 
 # 2. Print a list of completed tasks
 
-# def completed_tasks(list):
-#     for task in list:
-#         if task["completed"]:
-#             print(task)
+def completed_tasks(list):
+    for task in list:
+        if task["completed"]:
+            print(task)
 
 # completed_tasks(tasks)
 
 # 3. Print a list of all task descriptions
 
-# def find_description(list):
-#     descriptions = []
-#     for task in list:
-#         descriptions.append(task["description"])
-#     return descriptions
+def find_description(list):
+    descriptions = []
+    for task in list:
+        descriptions.append(task["description"])
+    return descriptions
 
 # print(find_description(tasks))
 
 # 4. Print a list of tasks where time_taken is at least a given time
 
-# def minumum_time(time, list):
-#     tasks_that_meet_time_target = []
-#     for task in list:
-#         if task["time_taken"] >= 30:
-#             tasks_that_meet_time_target.append(task)
-#     return tasks_that_meet_time_target
+def minumum_time(time, list):
+    tasks_that_meet_time_target = []
+    for task in list:
+        if task["time_taken"] >= 30:
+            tasks_that_meet_time_target.append(task)
+    return tasks_that_meet_time_target
 
 
 # print(minumum_time(30, tasks))
 
 # 5. Print any task with a given description
 
-# def input_description(input, list):
-#     for task in list:
-#         if task["description"] == input:
-#             print(task)
+def input_description(input, list):
+    for task in list:
+        if task["description"] == input:
+            print(task)
 
 # input_description("Wash Dishes", tasks)
 
 # 6. Given a description update that task to mark it as complete.
 
-# def update_completed_task(input, list):
-#     for task in list:
-#         if task["description"] == input:
-#             task["completed"] = True
-#     return list
+def update_completed_task(input, list):
+    for task in list:
+        if task["description"] == input:
+            task["completed"] = True
+    return list
 
 # print(update_completed_task("Wash Dishes", tasks))
 
 # 7. Add a task to the list
 
-# def add_new_task(new_task, list):
-#     list.append(new_task)
+def add_new_task(new_task, list):
+    list.append(new_task)
 
 # new_task = { 
 #     "description": "Make Bed", "completed": False, "time_taken": 5 }
@@ -104,11 +104,32 @@ def user_menu():
         print("7: Add a new Task to list")
         print("M or m: Display this menu")
         print("Q or q: Quit")
-        print("You selected " + user_input)
+        return(user_input)
     
-user_menu()
+# user_menu()
 
 # 9. Call the appropriate function depending on the users choice.
+
+def user_choice(list):
+    user_input = user_menu()
+    if user_input == "1":
+        print(list)
+    elif user_input == "2":
+        uncompleted_tasks(list)
+    elif user_input == "3":
+        completed_tasks(list)
+    elif user_input == "4":
+        update_completed_task(input, list)
+    elif user_input == "5":
+        minumum_time(time, list)
+    elif user_input == "6":
+        input_description(input, list)
+    elif user_input == "7":
+        add_new_task(new_task, list)
+
+user_choice(tasks)
+        
+
 #  7  week_1/day_4/functions_lab_2/start_code/task_list.py 
 # @@ -0,0 +1,7 @@
 # tasks = [
